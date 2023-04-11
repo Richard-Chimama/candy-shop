7c2fb1bd6d838ca7be8d1e998512b5354a54b340
Richard Chimama <rich.chime@gmail.com>
2023-04-11 12:34:26 +0200

    added the dependencies

--- client-template/src/components/Nav.jsx
+++ client-template/src/components/Nav.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { Link} from 'react-router-dom'
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav>
+      <Link to="/"></Link>
+
+
+    </nav>
   )
 }
 
