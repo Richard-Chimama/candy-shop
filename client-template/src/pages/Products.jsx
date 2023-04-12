d885ec430d27435ea9d228fa5610e2b809c7b1be
Nikola Zovko <nikolajoe95@gmail.com>
2023-04-12 20:44:40 +0200

    stepp2

--- client-template/src/pages/Products.jsx
+++ client-template/src/pages/Products.jsx
@@ -1,8 +1,12 @@
 import React, { useEffect, useState } from 'react'
+import { Link} from 'react-router-dom'
+
 
 const Products = () => {
   
   const [puns, setPuns] = useState([]);
+  
+   
 
   useEffect (() => {
     fetchData()
@@ -25,21 +29,28 @@ const Products = () => {
   return (
     <>
 
-     <h1>HELLO WORLD 1234 </h1> 
+     <h1>HELLO WORLD Nikola</h1> 
 
      <section>
+
       {
+
         puns.map(pun => {
-          return <article>
-            <img src={pun.image} alt='here shoul be iamge'></img>
-            <p>{pun.title}</p>
-            <p>{pun.price}</p>
-            <p>Stock i wil do later</p>
-            <button>Add to the card</button>
-            <p>In stock</p>
-            <p>READ MORE LINK will do later</p>
+          return <article key={pun['_id']}> 
+
+                      <img src={pun.image} alt='here should be a image'></img>
+                      <h2>{pun.title}</h2>
+                      <p>{pun.price} SEK</p>
+                      <input type='number' value="0" />
+                      <button>Add to cart</button>
+                      <p>In Stock</p>
+
+                       <Link to={'/product/' + pun['_id'] }>
+                          <i>Read more...</i>
+                       </Link>
 
           </article>
+
         })
 
       }
