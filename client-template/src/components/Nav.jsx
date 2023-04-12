e646c5f05e89e934f50eac7819205737bf0c3e78
Richard Chimama <rich.chime@gmail.com>
2023-04-13 00:36:21 +0200

    added cart functionatlity

--- client-template/src/components/Nav.jsx
+++ client-template/src/components/Nav.jsx
@@ -1,34 +1,45 @@
-import React from 'react'
-import { Link} from 'react-router-dom'
-import {FaShoppingCart} from "react-icons/fa"
-import styled from "styled-components"
+import React, { useContext, useState } from "react";
+import { Link } from "react-router-dom";
+import { FaShoppingCart } from "react-icons/fa";
+import styled from "styled-components";
+import LocalData from "../LocalData";
+import CartPanel from "./Cart";
 
 const Nav = () => {
+  const { cartData, setCartData } = useContext(LocalData);
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <Container>
-      <NavLink to='/'>Products</NavLink>
-      <NavLink to='/admin'>Admin</NavLink>
-      <Cart>
-        <FaShoppingCart  color="#333" size={25} />
-        <span>(0)</span>
+      <NavLink to="/">Products</NavLink>
+      <NavLink to="/admin">Admin</NavLink>
+      <Cart onClick={() => setIsOpen(!isOpen)}>
+        <FaShoppingCart color="#333" size={25} />
+        <span>({cartData.length})</span>
       </Cart>
+      {isOpen && (
+        <CartPanel
+          data={cartData}
+          resetData={setCartData}
+          closeCart={setIsOpen}
+        />
+      )}
     </Container>
-  )
-}
+  );
+};
 
 const NavLink = styled(Link)`
   text-decoration: none;
   font-family: Roboto;
   font-size: 22px;
   color: #333;
-`
+`;
 
 const Cart = styled.div`
   display: flex;
   align-items: center;
   gap: 5px;
   margin-left: auto;
-`
+`;
 
 const Container = styled.nav`
   display: flex;
@@ -37,6 +48,6 @@ const Container = styled.nav`
   gap: 50px;
   width: 80%;
   margin: 0 auto;
-`
+`;
 
-export default Nav
+export default Nav;
