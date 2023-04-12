9d2fb9cd68847628e28277188e0a81f5a53054fa
Richard Chimama <rich.chime@gmail.com>
2023-04-12 13:01:42 +0200

    updated the navigation bar and added an icon

--- client-template/src/components/Nav.jsx
+++ client-template/src/components/Nav.jsx
@@ -1,14 +1,42 @@
 import React from 'react'
 import { Link} from 'react-router-dom'
+import {FaShoppingCart} from "react-icons/fa"
+import styled from "styled-components"
 
 const Nav = () => {
   return (
-    <nav>
-      <Link to='/'>Products</Link>
-      <Link to='/admin'>Admin</Link>
-      <Link to='/checkout'>Checkout</Link>
-    </nav>
+    <Container>
+      <NavLink to='/'>Products</NavLink>
+      <NavLink to='/admin'>Admin</NavLink>
+      <Cart>
+        <FaShoppingCart  color="#333" size={25} />
+        <span>(0)</span>
+      </Cart>
+    </Container>
   )
 }
+
+const NavLink = styled(Link)`
+  text-decoration: none;
+  font-family: Roboto;
+  font-size: 22px;
+  color: #333;
+`
+
+const Cart = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 5px;
+  margin-left: auto;
+`
+
+const Container = styled.nav`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 50px;
+  width: 80%;
+  margin: 0 auto;
+`
 
 export default Nav
