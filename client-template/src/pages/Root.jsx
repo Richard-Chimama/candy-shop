94fccbeafcb98bff9ad3958122fd9e280b884487
Richard Chimama <rich.chime@gmail.com>
2023-04-13 00:40:42 +0200

    sample data

--- client-template/src/pages/Root.jsx
+++ client-template/src/pages/Root.jsx
@@ -1,41 +1,43 @@
-import React, {useState} from 'react'
-import Nav from '../components/Nav'
-import { Outlet } from 'react-router-dom'
-import LocalData from '../LocalData'
-
-
+import React, { useState } from "react";
+import Nav from "../components/Nav";
+import { Outlet } from "react-router-dom";
+import LocalData from "../LocalData";
 
 const Root = () => {
+  //This is just a sample data, it will be deleted at the end
   const data = [
     {
       title: "initial",
-      image: "https://img.freepik.com/free-vector/sweet-candies-flat-elements-set_1284-34170.jpg?w=996&t=st=1681334045~exp=1681334645~hmac=2e8572685288d3f39519424b3d7da57de259748860c6fcac26e56a806e4b019a",
+      image:
+        "https://img.freepik.com/free-vector/sweet-candies-flat-elements-set_1284-34170.jpg?w=996&t=st=1681334045~exp=1681334645~hmac=2e8572685288d3f39519424b3d7da57de259748860c6fcac26e56a806e4b019a",
       price: "10",
-      item: 1
-  },
-  {
-    title: "initial",
-    image: "https://img.freepik.com/free-vector/sweet-candies-flat-elements-set_1284-34170.jpg?w=996&t=st=1681334045~exp=1681334645~hmac=2e8572685288d3f39519424b3d7da57de259748860c6fcac26e56a806e4b019a",
-    price: "15",
-    item: 1
-},{
-  title: "Candy Candy Candy",
-  image: "https://img.freepik.com/free-vector/sweet-candies-flat-elements-set_1284-34170.jpg?w=996&t=st=1681334045~exp=1681334645~hmac=2e8572685288d3f39519424b3d7da57de259748860c6fcac26e56a806e4b019a",
-  price: "20",
-  item: 1
-}
-]
-  const [cartData, setCartData] = useState(data)
+      item: 1,
+    },
+    {
+      title: "initial",
+      image:
+        "https://img.freepik.com/free-vector/sweet-candies-flat-elements-set_1284-34170.jpg?w=996&t=st=1681334045~exp=1681334645~hmac=2e8572685288d3f39519424b3d7da57de259748860c6fcac26e56a806e4b019a",
+      price: "15",
+      item: 2,
+    },
+    {
+      title: "Candy Candy Candy",
+      image:
+        "https://img.freepik.com/free-vector/sweet-candies-flat-elements-set_1284-34170.jpg?w=996&t=st=1681334045~exp=1681334645~hmac=2e8572685288d3f39519424b3d7da57de259748860c6fcac26e56a806e4b019a",
+      price: "20",
+      item: 1,
+    },
+  ];
+  const [cartData, setCartData] = useState(data);
   return (
-    <LocalData.Provider value={{cartData, setCartData}}>
-        <Nav />
+    <LocalData.Provider value={{ cartData, setCartData }}>
+      <Nav />
 
-    <section>
+      <section>
         <Outlet />
-    </section>
-      
+      </section>
     </LocalData.Provider>
-  )
-}
+  );
+};
 
-export default Root
+export default Root;
