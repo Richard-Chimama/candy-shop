7ae3658e305e6a378672b9d6d1b74441a7242b1b
nataliasundstrm <97972459+nataliasundstrm@users.noreply.github.com>
2023-04-14 20:10:09 +0200

    Basic-style on footer

--- client-template/src/components/Footer.jsx
+++ client-template/src/components/Footer.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.section `
+  display: grid;
+  grid-template-columns: minmax(30px, 1fr) [inner-start] minmax(0, 70rem) [inner-end] minmax(30px, 1fr);
+  height: 300px;
+`;
+  
+const ContainerInner = styled.div `
+  grid-column: inner;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const CopyRight = styled.p `
+  font-family: roboto;
+`;
+
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <Container>
+      <ContainerInner>
+        <div>
+          <CopyRight>&copy; Copyright, Candy shop 2023</CopyRight>
+        </div>
+      </ContainerInner>
+    </Container>
   )
 }
 
