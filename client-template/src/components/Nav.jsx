432076f09f503683f611523b3a17d3920c795348
Richard Chimama <rich.chime@gmail.com>
2023-04-16 09:46:57 +0200

    added close cart prompt by clicking anywhere

--- client-template/src/components/Nav.jsx
+++ client-template/src/components/Nav.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useRef, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { FaShoppingCart } from "react-icons/fa";
 import styled from "styled-components";
@@ -8,14 +8,34 @@ import CartPanel from "./Cart";
 const Nav = () => {
   const { cartData, setCartData } = useContext(LocalData);
   const [isOpen, setIsOpen] = useState(false);
+  const promptRef = useRef(null);
+
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (promptRef.current && !promptRef.current.contains(event.target)) {
+        setIsOpen(false);
+      }
+    }
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [promptRef]);
+
   return (
-    <Container>
+    <Container ref={promptRef}>
       <div>
         <NavLink to="/">Products</NavLink>
         <NavLink to="/admin">Admin</NavLink>
       </div>
 
-      <Cart onClick={() => setIsOpen(!isOpen)}>
+      <Cart
+        onClick={() => {
+          setIsOpen(!isOpen);
+        }}
+      >
         <FaShoppingCart color="#FFFFF" size={25} />
         <span>({cartData.length})</span>
       </Cart>
@@ -45,11 +65,11 @@ const Cart = styled.div`
   gap: 5px;
   color: white;
 `;
-  
+
 const Container = styled.nav`
   height: 96px;
   display: flex;
-  justify-content: space-between; 
+  justify-content: space-between;
   align-items: center;
 `;
 
