47483792f623fea396701e56564e30b643827c06
nataliasundstrm <97972459+nataliasundstrm@users.noreply.github.com>
2023-04-19 11:52:16 +0200

    Add style to product-page

--- client-template/src/pages/Product.jsx
+++ client-template/src/pages/Product.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState, useContext } from "react";
-import { Link, useParams } from "react-router-dom";
+import { Link, useParams, useNavigate } from "react-router-dom";
 import LocalData from "../LocalData";
 import InputComponent from "../components/InputComponent";
 import ShowRoute from "../components/ShowRoute";
 import styled from "styled-components";
+import Colors from "../Theme";
+import { motion } from "framer-motion"
 
 
 
@@ -91,38 +93,31 @@ const Product = () => {
 
       <ShowRoute
         route={"/products/product"}
+        navigateTO={"/"}
        />
 
 
     <Div>
+
           <Bild src={product.image}   alt="here should be a image" />
 
       <Kontent>
 
         
-          <content key={product._id}>
+          <div key={product._id}>
               <h2>{product.title}</h2>
               <p>{product.price} SEK</p>
               <p>{product.description}</p>
               <p>Stock:{product.stock}</p>
 
+            <div>
               <InputComponent onDataReceived={handleInputComponent} />
-          <br />
-              <br />
-              <br />
 
-              <Buton onClick={() => addToLocalData(product)}>Add to cart</Buton> 
+              <Buton as={motion.button} whileHover={{ scale: 1.01, backgroundColor: Colors.color3, color: Colors.white }} onClick={() => addToLocalData(product)}>Add to cart</Buton> 
+            </div>
               
-              <br />
-              <br />
 
-            <Link to={"/"}>
-
-               <p> &#129120; Go back</p>
-
-            </Link>
-
-          </content>
+          </div>
      
       </Kontent>
 
@@ -133,47 +128,38 @@ const Product = () => {
   );
 };
 
-
-
-
 const Bild = styled.img`
-  width: 40%;
-  height: 40%;
+  height: 100%;
+
 `;
 
 const Kontent = styled.div`
 display: flex;
 justify-content: left;
 align-items: start;
+margin: 0 auto;
+
+p, 
+h2 {
+  font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+  margin-bottom: 10px;
+}
 `;
 
 const Div = styled.div`
 display: inline-flex;
 align-items: center;
-justify-content: space-evenly;`;
+justify-content: space-evenly;
+padding-top: 1rem;
+gap: 3rem;
+`;
 
-
-
-const Buton = styled.button`
-box-shadow: 0px 0px 10px 0px #eb0ece;
-	background-color:#f794e5;
-	border-radius:28px;
-	border:1px solid #ffffff;
-	display:inline-block;
-	cursor:pointer;
-	color:#ffffff;
-	font-family:Arial;
-	font-size:17px;
-	padding:16px 31px;
-	text-decoration:none;
-
-&:hover {
-  background-color:#de74cb;
-}
-
-&:active {
-  position:relative;
-	top:1px;
+const Buton = styled(motion.button)`
+  border: 0;
+  padding: 10px;
+  border-radius: 8px;
+  margin: 10px 0 10px 10px;
+  font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
 `;
 
 
